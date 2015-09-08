a8f5c51e6e5ecb2ba5a14d5b80aeba3be98ea906
Victor Borshchov <victor.borshchov@gmail.com>
2015-09-08 23:14:45 +0300

    edit set_page method

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -1,41 +1,18 @@
 class ReceiptsController < ApplicationController
-  before_filter :set_page, only:[:index, :show]
+  before_filter :set_page, only:[:index, :show, :search]
 
   def index
     @receipts = []
     @page.css("script, .topic-user-info, #ingridient-header, .share-buttons, .action, div[id^='div-gpt-ad'], .content>.clear>a").remove # remove all unneeded content
-    @page.css(".topic").each_with_index do |t, index|
-      t.css(".voting-border").remove
-      t[:id] = "to-top" if index == 0
-      @receipts << t.to_html
+    @page.css(".topic").each_with_index do |topic, index|
+      topic.css(".voting-border").remove
+      @receipts << topic.to_html
     end
-    @pagination = @page.css('#pagination').to_html
+    # @pagination = @page.css('#pagination').to_html
 
     # right menu
-    right_menu = Nokogiri::HTML::DocumentFragment.parse(@page.css('#block-best-topics').to_s)
-    right_menu.css('.best-item-r a:nth-child(2) ,.best-item-r a:nth-child(3)').remove
-    puts "*****************************"
-    right_menu.css('.best-item-r').each {|node| puts node.content}
-    puts "*****************************"
-    @right_menu = (   "<li><label>Найкращі рецепти</label></li>" +
-                        "<ul class='tabs' data-tab>
-                          <li class='tab-title active'><a href='#panel1'>#{right_menu.css('.tabs li:nth-child(1)').text}</a></li>
-                          <li class='tab-title'><a href='#panel2'>#{right_menu.css('.tabs li:nth-child(2)').text}</a></li>
-                          <li class='tab-title'><a href='#panel3'>#{right_menu.css('.tabs li:nth-child(3)').text}</a></li>
-                        </ul>
-                        <div class='tabs-content'>
-                          <div class='content active' id='panel1'>
-                            #{right_menu.css('.topics-week').to_html}
-
-                          </div>
-                          <div class='content' id='panel2'>
-                            #{right_menu.css('.topics-month').to_html}
-                          </div>
-                          <div class='content' id='panel3'>
-                            #{right_menu.css('.topics-total').to_html}
-                          </div>
-                        </div>"
-                    ).html_safe
+    @right_menu = Nokogiri::HTML::DocumentFragment.parse(@page.css('#block-best-topics').to_s)
+    @right_menu.css('.best-item-r a:nth-child(2) ,.best-item-r a:nth-child(3)').remove
   end
 
   def show
@@ -44,6 +21,7 @@ class ReceiptsController < ApplicationController
     img          = @page.css('#topic-show-avatar').to_html.html_safe
     top_tags     = @page.css('.topic ul.tags.top-tags').to_html.html_safe
     tags         = @page.css('#view-topic ul.tags')[1].to_html.html_safe
+    @ingredients = @page.css('table.ingredients .ingridient-group-title:first').remove
     @ingredients = @page.css('table.ingredients').to_html.html_safe
     content      = @page.css('#view-topic .content').to_html.html_safe
     @receipt = Receipt.new(
@@ -58,21 +36,21 @@ class ReceiptsController < ApplicationController
 
   def search
     @receipts = []
-    link = URI::escape("#{params[:link]}?q=#{params[:q]}")
-    page = Nokogiri::HTML(open(link))
-    page.css(".topic a").map do |a|
-      if a["href"] =~ /\.html\z/
-        a["href"] = "/receipts/show?link=" + a["href"].to_s
-      else
-        a["href"] = "/receipts/?link=" + a["href"].to_s
-      end
-    end
-    page.css("#pagination a").map {|a| a["href"] = "/receipts/search?link=" + a["href"].to_s[/.+(?=\?)/] + "&q=" + "#{params[:q]}" }
-    page.css(".topic").each do |topic|
+    # link = URI::escape("#{params[:link]}?q=#{params[:q]}")
+    # page = Nokogiri::HTML(open(link))
+    # @page.css(".topic a").map do |a|
+    #   if a["href"] =~ /\.html\z/
+    #     a["href"] = "/receipts/show?link=" + a["href"].to_s
+    #   else
+    #     a["href"] = "/receipts/?link=" + a["href"].to_s
+    #   end
+    # end
+    @page.css("#pagination a").map {|a| a["href"] = "/receipts/search?link=" + a["href"].to_s[/.+(?=\?)/] + "&q=" + "#{params[:q]}" }
+    @page.css(".topic").each do |topic|
       topic.css(".voting-border, .action").remove
       @receipts << topic.to_html.html_safe
     end
-    @pagination = page.css('#pagination').to_html
+    # @pagination = @age.css('#pagination').to_html
   end
 
   def tag
@@ -85,10 +63,10 @@ class ReceiptsController < ApplicationController
 
   private
 
-    def set_search_params
-      page = Nokogiri::HTML(open("http://cookorama.net/uk/ingredients"))
-      @ingredients = page.css('.ingridients-list ')
-    end
+    # def set_search_params
+    #   page = Nokogiri::HTML(open("http://cookorama.net/uk/ingredients"))
+    #   @ingredients = page.css('.ingridients-list ')
+    # end
 
     def set_page
       link = params[:link] =~ /^http:\/\/cookorama\.net/ && params[:link] || "http://cookorama.net"
@@ -98,7 +76,8 @@ class ReceiptsController < ApplicationController
       #         else
       #           "http://cookorama.net"
       #         end
-      link = URI::escape(link)
+      link = URI::escape("#{link}?q=#{params[:q]}")
+      # link = URI::escape(link)
       @page = Nokogiri::HTML(open(link))
       @page.css(".topic a").map do |a|
         if a["href"] =~ /\.html\z/
@@ -108,6 +87,7 @@ class ReceiptsController < ApplicationController
         end
       end
       @title = @page.css(".title span").text
+      @pagination = @page.css('#pagination').to_html
     end
 
 end

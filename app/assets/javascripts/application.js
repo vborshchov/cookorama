85552999005fec8b443f5f1706828156734b25b2
Victor Borshchov <victor.borshchov@gmail.com>
2015-09-12 23:47:11 +0300

    Add back-to-top button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,6 +35,32 @@ ready = function() {
   // Initialize height
   $(document).trigger('resize');
 
+  // --------------------- START Scroll to top button ------------------------------
+  var offset = 300,
+    //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+    offset_opacity = 1200,
+    //duration of the top scrolling animation (in ms)
+    scroll_top_duration = 700,
+    //grab the "back to top" link
+    $back_to_top = $('.cd-top');
+
+  //hide or show the "back to top" link
+  $(window).scroll(function(){
+    ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+    if( $(this).scrollTop() > offset_opacity ) {
+      $back_to_top.addClass('cd-fade-out');
+    }
+  });
+
+  //smooth scroll to top
+  $back_to_top.on('click', function(event){
+    event.preventDefault();
+    $('body,html').animate({
+      scrollTop: 0 ,
+      }, scroll_top_duration
+    );
+  });
+  // --------------------- END Scroll to top button ------------------------------
 };
 
 $(document).ready(ready);

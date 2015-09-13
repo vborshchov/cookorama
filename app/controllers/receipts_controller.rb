4a6d1d37fc1e42526dac0e6bfbc55b86188b68dd
Victor Borshchov <victor.borshchov@gmail.com>
2015-09-14 01:30:47 +0300

    use Mechanize to parse pages

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -3,37 +3,37 @@ class ReceiptsController < ApplicationController
   before_filter :set_receipts, only:[:index, :search, :tag, :filter]
 
   def index
-    @page.css("script, .topic-user-info, #ingridient-header, .share-buttons, .action, div[id^='div-gpt-ad'], .content>.clear>a").remove # remove all unneeded content
-    @right_menu = @page.css("#block-best-topics")
-    @right_menu.css(".best-item-r a:nth-child(2) ,.best-item-r a:nth-child(3)").remove
+    @page.search("script, .topic-user-info, #ingridient-header, .share-buttons, .action, div[id^='div-gpt-ad'], .content>.clear>a").remove # remove all unneeded content
+    @right_menu = @page.search("#block-best-topics")
+    @right_menu.search(".best-item-r a:nth-child(2) ,.best-item-r a:nth-child(3)").remove
     @right_menu = "<li><label>Найкращі рецепти</label></li>
                    <ul class='tabs' data-tab>
-                     <li class='tab-title active'><a href='#panel1'>#{@right_menu.css('.tabs li:nth-child(1)').text }</a></li>
-                     <li class='tab-title'><a href='#panel2'>#{@right_menu.css('.tabs li:nth-child(2)').text }</a></li>
-                     <li class='tab-title'><a href='#panel3'>#{@right_menu.css('.tabs li:nth-child(3)').text }</a></li>
+                     <li class='tab-title active'><a href='#panel1'>#{@right_menu.search('.tabs li:nth-child(1)').text }</a></li>
+                     <li class='tab-title'><a href='#panel2'>#{@right_menu.search('.tabs li:nth-child(2)').text }</a></li>
+                     <li class='tab-title'><a href='#panel3'>#{@right_menu.search('.tabs li:nth-child(3)').text }</a></li>
                    </ul>
                    <div class='tabs-content'>
                      <div class='content active' id='panel1'>
-                       #{@right_menu.css('.topics-week').to_html.html_safe }
+                       #{@right_menu.search('.topics-week').to_html.html_safe }
                      </div>
                      <div class='content' id='panel2'>
-                       #{@right_menu.css('.topics-month').to_html.html_safe }
+                       #{@right_menu.search('.topics-month').to_html.html_safe }
                      </div>
                      <div class='content' id='panel3'>
-                       #{@right_menu.css('.topics-total').to_html.html_safe }
+                       #{@right_menu.search('.topics-total').to_html.html_safe }
                      </div>
                    </div>".html_safe
   end
 
   def show
-    @page.css("#view-topic .content .clear").remove
-    title        = @page.css("h1.title").text
-    img          = @page.css("#topic-show-avatar").to_html.html_safe
-    top_tags     = @page.css(".topic ul.tags.top-tags").to_html.html_safe
-    tags         = @page.css("#view-topic ul.tags")[1].to_html.html_safe
-    @ingredients = @page.css("table.ingredients .ingridient-group-title:first").remove
-    @ingredients = @page.css("table.ingredients").to_html.html_safe
-    content      = @page.css("#view-topic .content").to_html.html_safe
+    @page.search("#view-topic .content .clear").remove
+    title        = @page.search("h1.title").text
+    img          = @page.search("#topic-show-avatar").to_html.html_safe
+    top_tags     = @page.search(".topic ul.tags.top-tags").to_html.html_safe
+    tags         = @page.search("#view-topic ul.tags")[1].to_html.html_safe
+    @ingredients = @page.search("table.ingredients .ingridient-group-title:first").remove
+    @ingredients = @page.search("table.ingredients").to_html.html_safe
+    content      = @page.search("#view-topic .content").to_html.html_safe
     @receipt = Receipt.new(
       title:       title,
       img:         img,
@@ -42,65 +42,65 @@ class ReceiptsController < ApplicationController
       ingredients: @ingredients,
       content:     content
     )
-    @comments = @page.css(".comments>div[id^=comment_id_], .comments .header h3")
-    @comments.css("a").each{|a| a["href"]="#"}
-    @comments.css(".info ul li:nth-child(n+2)").remove
+    @comments = @page.search(".comments>div[id^=comment_id_], .comments .header h3")
+    @comments.search("a").each{|a| a["href"]="#"}
+    @comments.search(".info ul li:nth-child(n+2)").remove
   end
 
   def search
-    @page.css('#pagination a').map {|a| a["href"] = a["href"].gsub("/receipts?", "/receipts/search?")}
-    @quantity = @page.css('#content .block-nav li:first').text
-    @right_menu = @page.css('#sidebar')
-    @right_menu.css("script, .cl .cr h2").remove
-    @selected_filters = @right_menu.css(".block-filter-selected").remove
-    @selected_filters.css(".remove").remove
+    @page.search('#pagination a').map {|a| a["href"] = a["href"].gsub("/receipts?", "/receipts/search?")}
+    @quantity = @page.search('#content .block-nav li:first').text
+    @right_menu = @page.search('#sidebar')
+    @right_menu.search("script, .cl .cr h2").remove
+    @selected_filters = @right_menu.search(".block-filter-selected").remove
+    @selected_filters.search(".remove").remove
     # discard all filters link
-    if @selected_filters.css("a span")[1] && @selected_filters.css("a span")[1].text =~ /відмінити/
-      @selected_filters.css("a")[0]["href"] = "#{receipts_search_path}?link=#{@selected_filters.css('a')[0]['href'].gsub('?', '&')}"
+    if @selected_filters.search("a span")[1] && @selected_filters.search("a span")[1].text =~ /відмінити/
+      @selected_filters.search("a")[0]["href"] = "#{receipts_search_path}?link=#{@selected_filters.search('a')[0]['href'].gsub('?', '&')}"
     end
     # right sideabr filters links
-    @right_menu.css(".filter-list ul li").each do |li|
+    @right_menu.search(".filter-list ul li").each do |li|
       link_parameters = {}
       link_parameters[:filters] = {"blog": []}
-      link_parameters[:filters][:blog] << li.css('a input')[0]['value']
-      unless li.css("ul").length == 0
-        li.css('ul input').each do |input|
+      link_parameters[:filters][:blog] << li.search('a input')[0]['value']
+      unless li.search("ul").length == 0
+        li.search('ul input').each do |input|
           link_parameters[:filters][:blog] << input["value"]
         end
       end
       if params[:filters]
         link_parameters[:filters][:blog] |= params[:filters][:blog]
       end
-      li.css("a")[0]["href"] = [receipts_search_path, params.merge(link_parameters).except("controller", "action").to_query].join("?")
+      li.search("a")[0]["href"] = [receipts_search_path, params.merge(link_parameters).except("controller", "action").to_query].join("?")
     end
   end
 
   def tag
-    @right_menu = @page.css('.block.tags').to_html.html_safe
+    @right_menu = @page.search('.block.tags').to_html.html_safe
   end
 
   def filter
     @selected_filters = {}
-    @page.css("form .filter-item").each do |filter|
-      filter.css("option").each {|opt| @selected_filters["#{filter['id']}"] = opt.text if (opt["selected"] && opt["value"]) }
+    @page.search("form .filter-item").each do |filter|
+      filter.search("option").each {|opt| @selected_filters["#{filter['id']}"] = opt.text if (opt["selected"] && opt["value"]) }
     end
-    @page.css('#sidebar form')[0].add_child("<input name='link' value='http://cookorama.net/uk/filter/' type='hidden'>")
-    @right_menu = @page.css('#sidebar form')[0]["action"] = "#{receipts_filter_path}"
-    @right_menu = @page.css('#sidebar form').to_html.html_safe
+    @page.search('#sidebar form')[0].add_child("<input name='link' value='http://cookorama.net/uk/filter/' type='hidden'>")
+    @right_menu = @page.search('#sidebar form')[0]["action"] = "#{receipts_filter_path}"
+    @right_menu = @page.search('#sidebar form').to_html.html_safe
   end
 
   private
 
     def set_receipts
       @receipts = []
-      @page.css(".topic").each do |topic|
-        topic.css(".voting-border, .action").remove
-        if topic.css(".topic-recipe")[0]
-          topic.css(".topic-recipe")[0].name = "ul"
-          topic.css(".topic-recipe")[0]["class"] = "topic-recipe small-block-grid-1 medium-block-grid-2"
-          topic.css(".topic-recipe-content")[0].name = "li"
-          topic.css(".topic-recipe-img")[0].name = "li"
-          topic.css(".topic-recipe").children.last.add_next_sibling(topic.css(".topic-recipe-content"))
+      @page.search(".topic").each do |topic|
+        topic.search(".voting-border, .action").remove
+        if topic.search(".topic-recipe")[0]
+          topic.search(".topic-recipe")[0].name = "ul"
+          topic.search(".topic-recipe")[0]["class"] = "topic-recipe small-block-grid-1 medium-block-grid-2"
+          topic.search(".topic-recipe-content")[0].name = "li"
+          topic.search(".topic-recipe-img")[0].name = "li"
+          topic.search(".topic-recipe").children.last.add_next_sibling(topic.search(".topic-recipe-content"))
         end
         @receipts << topic.to_html.html_safe
       end
@@ -111,14 +111,16 @@ class ReceiptsController < ApplicationController
       link_for_parse = "#{link}?#{params.reject{|e| e =~ /link|controller|action/ }.to_query}"
       link_for_parse = URI.escape(URI.unescape(link_for_parse))
       begin
-        @page = Nokogiri::HTML(open(link_for_parse))
+        agent = Mechanize.new
+        @page = agent.get(link_for_parse)
+        # @page = Nokogiri::HTML(open(link_for_parse))
       rescue
         link_for_parse = URI.unescape(link_for_parse)
         puts link_for_parse.gsub!("search/topics/", "search/comments/")
         link_for_parse = URI.escape(URI.unescape(link_for_parse))
         @page = Nokogiri::HTML(open(link_for_parse))
       end
-      @page.css(".topic a, .best-item a, #pagination a, .cloud a").map do |a|
+      @page.search(".topic a, .best-item a, #pagination a, .cloud a").map do |a|
         if a["href"] =~ /\.html\z/ && a["href"] =~ /\Ahttp:\/\/cookorama\.net/
           a["href"] = "#{receipts_show_path}?link=" + a["href"]
         else
@@ -132,8 +134,8 @@ class ReceiptsController < ApplicationController
           end
         end
       end
-      @title = @page.css(".title span").text
-      @pagination = @page.css('#pagination')
+      @title = @page.search(".title span").text
+      @pagination = @page.search('#pagination')
     end
 
 end

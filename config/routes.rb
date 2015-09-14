41023a1116e2e03d9191a415d76e6d31bc46a932
Victor Borshchov <victor.borshchov@gmail.com>
2015-09-14 23:46:48 +0300

    create User model with custom email validator

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  #get 'users/new'
+  get '/signup' => 'users#new'
 
   get 'receipts/show'
   get 'receipts/tag'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150907203739) do
+ActiveRecord::Schema.define(version: 20150914191115) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "receipts", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +25,13 @@ ActiveRecord::Schema.define(version: 20150907203739) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.text     "content"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

74ac3d7cdb1d4eee67df637ea0163e8193e0cf29
Victor Borshchov <victor.borshchov@gmail.com>
2015-09-15 22:48:04 +0300

    make signup form with clientside validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
 
-  validates :name, presence: true, length: { minimum: 3, maximum: 20 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 20 }, format: /\A[a-zA-Z0-9]\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, email: true
   validates :password, length: { minimum: 5 }
 
